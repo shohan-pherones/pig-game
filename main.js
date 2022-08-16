1b4163c5f06b328bd2b8193cd90b42c36fa87a85
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-08-16 16:16:19 +0600

    scores added

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
@@ -18,6 +20,14 @@ diceEl.classList.add("hidden");
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 // rolling dice
 btnRoll.addEventListener("click", function () {
@@ -37,8 +47,16 @@ btnRoll.addEventListener("click", function () {
       currentScore;
   } else {
     // switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener("click", function () {
+  // add current score to active players
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // check if it is >=100
+  // switch to next player
+  switchPlayer();
 });
